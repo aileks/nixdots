6e9f36eae1fcaec4be9adccee6edeb5679a95803
Kieran Pedrick <kieran@aileks.dev>
2026-09-04 20:13:47 -0400

    add back iosevka job limit

--- packages/iosevka-custom.nix
+++ packages/iosevka-custom.nix
@@ -1,5 +1,16 @@
 { iosevka }:
-iosevka.override {
+(iosevka.override {
   set = "Custom";
   privateBuildPlan = builtins.readFile ../config/iosevka/iosevka-custom.toml;
-}
+}).overrideAttrs
+  (_: {
+    preBuild = ''
+      # Limit font compilation to half the CPUs to avoid running out of memory.
+      jobs=$(( $(nproc) / 2 ))
+      [ "$jobs" -ge 1 ] || jobs=1
+      if [ "$NIX_BUILD_CORES" -gt 0 ] && [ "$NIX_BUILD_CORES" -lt "$jobs" ]; then
+        jobs=$NIX_BUILD_CORES
+      fi
+      export NIX_BUILD_CORES=$jobs
+    '';
+  })
