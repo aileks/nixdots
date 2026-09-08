fd3d3f5075ecb73bc095f96caed6102035ab982b
Kieran Pedrick <kieran@aileks.dev>
2026-09-08 14:58:05 -0400

    center wmenu on the focused output

--- packages/wmenu.nix
+++ packages/wmenu.nix
@@ -4,15 +4,20 @@
   makeWrapper,
 }:
 
+let
+  centeredWmenu = wmenu.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [ ../config/wmenu/center.patch ];
+  });
+in
 symlinkJoin {
   name = "wmenu-cinder-grove";
-  paths = [ wmenu ];
+  paths = [ centeredWmenu ];
   nativeBuildInputs = [ makeWrapper ];
   postBuild = ''
     for program in wmenu wmenu-run; do
       rm "$out/bin/$program"
-      makeWrapper "${wmenu}/bin/$program" "$out/bin/$program" \
-        --add-flags "-f 'Iosevka Nerd Font 16' -l 8 -N '#131210ee' -n '#BBB3A9' -M '#D9A441ee' -m '#131210' -S '#D9A441ee' -s '#131210'"
+      makeWrapper "${centeredWmenu}/bin/$program" "$out/bin/$program" \
+        --add-flags "-c -f 'Iosevka Nerd Font 16' -l 8 -N '#131210ee' -n '#BBB3A9' -M '#D9A441ee' -m '#131210' -S '#D9A441ee' -s '#131210'"
     done
   '';
   inherit (wmenu) meta;
